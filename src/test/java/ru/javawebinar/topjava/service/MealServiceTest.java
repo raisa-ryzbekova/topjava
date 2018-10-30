07ed2fc765ed7daf859af6c8d4dfe18073ae8a39
Raisa Ryzbekova <raisa.ryzbekova@gmail.com>
2018-10-30 12:48:22 +0700

    make Hibernate entity from Meal, implement and test JpaMealRepositoryImpl, add @Rule in MealServiceTest (exception check, execution time output for every test, execution time output for all tests in the end of the class)

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,15 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,7 +21,10 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -25,6 +36,28 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
+    private static final Logger log = LoggerFactory.getLogger(MealServiceTest.class);
+    private static List<String> testTimeData = new ArrayList();
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Rule
+    public TestWatcher watcherTest = new TestWatcher() {
+        private long startTime;
+
+        @Override
+        protected void starting(Description description) {
+            startTime = System.currentTimeMillis();
+        }
+
+        @Override
+        protected void finished(Description description) {
+            long testTimeExecute = System.currentTimeMillis() - startTime;
+            log.info("test executed in {} ms", testTimeExecute);
+            testTimeData.add("\nTest name: " + description.getMethodName() + ", time: " + testTimeExecute + " ms");
+        }
+    };
 
     static {
         SLF4JBridgeHandler.install();
@@ -41,6 +74,13 @@ public class MealServiceTest {
 
     @Test(expected = NotFoundException.class)
     public void deleteNotFound() throws Exception {
+        service.delete(MEAL1_ID, 1);
+    }
+
+    @Test
+    public void deleteNotFoundExceptionCheck() {
+        exception.expect(NotFoundException.class);
+        exception.expectMessage(containsString("Not found entity with id=" + MEAL1_ID));
         service.delete(MEAL1_ID, 1);
     }
 
@@ -63,6 +103,13 @@ public class MealServiceTest {
     }
 
     @Test
+    public void getNotFoundExceptionCheck() {
+        exception.expect(NotFoundException.class);
+        exception.expectMessage(containsString("Not found entity with id=" + MEAL1_ID));
+        service.get(MEAL1_ID, ADMIN_ID);
+    }
+
+    @Test
     public void update() throws Exception {
         Meal updated = getUpdated();
         service.update(updated, USER_ID);
@@ -71,6 +118,13 @@ public class MealServiceTest {
 
     @Test(expected = NotFoundException.class)
     public void updateNotFound() throws Exception {
+        service.update(MEAL1, ADMIN_ID);
+    }
+
+    @Test
+    public void updateNotFoundExceptionCheck() {
+        exception.expect(NotFoundException.class);
+        exception.expectMessage(containsString("Not found entity with id=" + MEAL1_ID));
         service.update(MEAL1, ADMIN_ID);
     }
 
@@ -84,5 +138,12 @@ public class MealServiceTest {
         assertMatch(service.getBetweenDates(
                 LocalDate.of(2015, Month.MAY, 30),
                 LocalDate.of(2015, Month.MAY, 30), USER_ID), MEAL3, MEAL2, MEAL1);
+    }
+
+    @AfterClass
+    public static void outTestTimeData() {
+        log.info("\n-------------------------------\n");
+        log.info("" + testTimeData);
+        log.info("\n-------------------------------\n");
     }
 }
